67eb134741039d6cc0614e06bfcb216b082b77c7
Boyd van Oosterhout <boydvanoosterhout@outlook.com>
2025-05-08 20:37:53 +0200

    Added tests for custom arguments

---
+++ Implementation/Liman.Tests/CustomArgumentTests.cs
@@ -0,0 +1,71 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liman.Tests
+{
+    public class CustomArgumentTests
+    {
+        ILimanServiceCollection serviceCollection;
+
+        public CustomArgumentTests()
+        {
+            serviceCollection = LimanFactory.CreateServiceCollection();
+            serviceCollection.Add(typeof(MyServiceWithCustomParameter), ServiceImplementationLifetime.Transient);
+        }
+
+        [Fact]
+        public void CustomParameter_WhenPassed_Succeeds()
+        {
+            // Arrange
+            var serviceProvider = LimanFactory.CreateServiceProvider(serviceCollection);
+
+            // Act
+            var service = serviceProvider.GetRequiredService<MyServiceWithCustomParameter>("TestValue");
+
+            // Assert
+            service.CustomArgument.Should().Be("TestValue");
+        }
+
+        [Fact]
+        public void CustomParameter_WhenNotPassed_ThrowsException()
+        {
+            // Arrange
+            var serviceProvider = LimanFactory.CreateServiceProvider(serviceCollection);
+
+            // Act
+            var action = () => serviceProvider.GetRequiredService<MyServiceWithCustomParameter>();
+
+            // Assert
+            action.Should().Throw<LimanException>();
+        }
+
+        [Fact]
+        public void CustomParameter_WhenTooManyPassed_ThrowsException()
+        {
+            // Arrange
+            var serviceProvider = LimanFactory.CreateServiceProvider(serviceCollection);
+
+            // Act
+            var action = () => serviceProvider.GetRequiredService<MyServiceWithCustomParameter>("Argument1", "Argument2");
+
+            // Assert
+            action.Should().Throw<LimanException>();
+        }
+
+        public class MyServiceWithCustomParameter
+        {
+            public MyServiceWithCustomParameter(IServiceProvider dependency, [NoInjection] string customArgument)
+            {
+                Dependency = dependency;
+                CustomArgument = customArgument;
+            }
+
+            public IServiceProvider Dependency { get; }
+            public string CustomArgument { get; }
+        }
+    }
+}
