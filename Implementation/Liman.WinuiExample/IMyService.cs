153d60c1fb3fbe74077ef8494f888ae556ef3cd7
Boyd van Oosterhout <6190802+BvOosterhout@users.noreply.github.com>
2025-05-11 10:55:23 +0200

    Added WinUiExample

---
+++ Implementation/Liman.WinuiExample/IMyService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liman.WinuiExample
+{
+    public interface IMyService
+    {
+    }
+}
